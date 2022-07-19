1172f43d1815cbf771630fafa8a389e101392811
MuhammadSidiq22 <muhammad_sidiq@example.com>
2022-07-19 16:37:12 +0700

    web-saya

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <img src='https://img2.pngdownload.id/20180715/chh/kisspng-business-organization-project-plan-innovation-trabajo-en-equipo-5b4b6c93d35b62.5049096415316696518657.jpg' width={80} height={70}></img>&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Profil SKPD</Nav.Link>
+            <Nav.Link href="#features">INFORMASI</Nav.Link>
+            <Nav.Link href="#pricing">GALERY</Nav.Link>
+            <Nav.Link href="#pricing">NEWS</Nav.Link>
+            <Nav.Link href="#pricing">CONTACT US</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
-}
 
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-class MyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      age: null,
-    };
-  }
-  mySubmitHandler = (event) => {
-    event.preventDefault();
-    let age = this.state.age;
-    if (!Number(age)) {
-      alert("Nama kamu terdata sebagai: " + this.state.nama);
-    }
-  }
-  myChangeHandler = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value;
-    this.setState({[nam]: val});
-  }
-  render() {
-    return (
-      <form onSubmit={this.mySubmitHandler}>
-      <h1>Mantab {this.state.nama}, sekarang kamu berumur {this.state.umur}</h1>
-      <p>Masukan Nama mu:</p>
-      <input
-        type='text'
-        name='nama'
-        onChange={this.myChangeHandler}
-      />
-      <p>Berapa umur sekarang:</p>
-      <input
-        type='text'
-        name='umur'
-        onChange={this.myChangeHandler}
-      />
-      <br/>
-      <br/>
-      <input type='submit' />
-      </form>
-    );
-  }
-}
 
-ReactDOM.render(<MyForm />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
